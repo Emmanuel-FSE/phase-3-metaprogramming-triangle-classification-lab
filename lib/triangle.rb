07fce0698d9230b8da0b0706ce24f60349dcddc9
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2023-02-15 22:00:27 +0300

    Assignment completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if @sides.any? {|side| side <= 0} || !triangle_inequality?
+      raise TriangleError
+    elsif @sides.uniq.size == 1
+      :equilateral
+    elsif @sides.uniq.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def triangle_inequality?
+    @sides.max < @sides.inject(:+) - @sides.max
+  end
 end
